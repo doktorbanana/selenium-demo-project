97227b5ff884f4c53bc583db656e2495f1fe151e
doktorbanana <peter.martin.hoevelmeyer@gmail.com>
2025-08-07 00:58:10 +0200

    Disable PW-leak-detection
    The Chrome Browser showed a pop-up about the password of the standard user was found in a breach. This pop-up caused tests to fail, because items were not clickable anymore. We disable the warning.

--- conftest.py
+++ conftest.py
@@ -7,8 +7,14 @@ import pytest
 def setup_browser():
     # Add headless mode for CI compatibility
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
     
-    driver = webdriver.Chrome(options=options)    
+    prefs = {
+        "profile.password_manager_leak_detection_enabled": False
+    }
+    options.add_experimental_option("prefs", prefs)
+    #options.add_argument("--headless=new")
+    
+    driver = webdriver.Chrome(options=options)
+    driver.delete_all_cookies()   
     yield driver
     driver.quit()
