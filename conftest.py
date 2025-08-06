45d3d95d515b84217cac12aa58f4ee575d725930
Peter Martin <53558347+doktorbanana@users.noreply.github.com>
2025-08-07 01:09:42 +0200

    fix: re-enable headless mode
    Headless Mode was turned off for local tests, but should be turned on for CI.

--- conftest.py
+++ conftest.py
@@ -12,7 +12,7 @@ def setup_browser():
         "profile.password_manager_leak_detection_enabled": False
     }
     options.add_experimental_option("prefs", prefs)
-    #options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     
     driver = webdriver.Chrome(options=options)
     driver.delete_all_cookies()   
