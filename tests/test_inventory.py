6c425c92fd9f8f1ec0f9276eebe8fea5cdb3f4b7
doktorbanana <peter.martin.hoevelmeyer@gmail.com>
2025-08-06 10:53:22 +0200

    feat: separate product_data

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,17 +1,19 @@
 from pages.login_page import LoginPage
+from utils.data_loader import load_csv
 import pytest
 
+products = load_csv("./test_data/products.csv")
 
-@pytest.mark.parametrize("product_name", [
-    ("Sauce Labs Backpack"),        # Backpack
-])
-
-def test_img_click(setup_browser, product_name):
+@pytest.mark.parametrize("product", products)
+def test_img_click(setup_browser, product):
     driver = setup_browser
     driver.get("https://saucedemo.com")
+
+    product_name = product["product_name"]
     
     login_page = LoginPage(driver)
     inventory_page = login_page.login_expect_success("standard_user", "secret_sauce")
     item_page = inventory_page.click_product_img(product_name)
 
     assert item_page.url_contains("item.html"), "Clicking product image did not redirect to item page"
+
