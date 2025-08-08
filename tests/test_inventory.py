334a7aad3dfe85d55c8c230998d5bb65d75c668d
doktorbanana <peter.martin.hoevelmeyer@gmail.com>
2025-08-08 20:46:45 +0200

    Use login-Fixture in test_inventory

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -9,29 +9,18 @@ custom_ids = [f"{row['custom_id']}" for row in products]
 
 @pytest.mark.parametrize("product", products, ids=custom_ids)
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
-def test_img_click(setup_browser, product):
-    driver = setup_browser
-    driver.get("https://saucedemo.com")
-
+def test_img_click(standard_login, product):
     product_name = product["product_name"]
-    
-    login_page = LoginPage(driver)
-    inventory_page = login_page.login_expect_success("standard_user", "secret_sauce")
+    inventory_page = standard_login
     item_page = inventory_page.click_product_img(product_name)
-
     assert item_page.url_contains("item.html"), "Clicking product image did not redirect to item page"
 
 
 @pytest.mark.parametrize("product", products, ids=custom_ids)
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
-def test_link_click(setup_browser, product):
-    driver = setup_browser
-    driver.get("https://saucedemo.com")
-
+def test_link_click(standard_login, product):
     product_name = product["product_name"]
-        
-    login_page = LoginPage(driver)
-    inventory_page = login_page.login_expect_success("standard_user", "secret_sauce")
+    inventory_page = standard_login
     item_page = inventory_page.click_product_link(product_name)
 
     assert item_page.url_contains("item.html"), "Clicking product link did not redirect to item page"
