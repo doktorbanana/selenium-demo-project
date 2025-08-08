124e6511e316474afaa0ac08e46a7754afc83999
doktorbanana <peter.martin.hoevelmeyer@gmail.com>
2025-08-08 12:14:47 +0200

    add screenshot to html-report
    To better debug why a test fails, we now make a screenshot of the browser, when a test fails. The screenshot is added to the html-report.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import pytest  
+import pytest
+import datetime
+import pytest_html
+import os
 
 
 @pytest.fixture(scope="function")
@@ -8,7 +11,32 @@ def setup_browser():
     # Add headless mode for CI compatibility
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
-    
+    options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)    
     yield driver
     driver.quit()
+
+@pytest.hookimpl(wrapper=True)
+def pytest_runtest_makereport(item, call):
+    # This hook is used to capture the test report and take a screenshot if the test fails
+    
+    report = yield
+
+    if report.when== "call" and report.failed:
+        driver = item.funcargs.get('setup_browser')
+        if driver:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            screenshot_path = f"./test_reports/screenshots"
+            screenshot_name = f"{item.name}_{timestamp}.png"
+
+            if not os.path.exists(screenshot_path):
+                os.mkdir(screenshot_path)
+
+            driver.save_screenshot(screenshot_path + "/" + screenshot_name)
+
+            extras = getattr(report, "extras", [])
+            extras.append(pytest_html.extras.image(screenshot_path + "/" + screenshot_name))
+            report.extras = extras
+
+
+    return report
