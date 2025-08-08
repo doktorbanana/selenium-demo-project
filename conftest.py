2123fb763fa1fe68a278caf25e997c764d8a3d35
doktorbanana <peter.martin.hoevelmeyer@gmail.com>
2025-08-08 22:42:27 +0200

    Fix #18 by disabling PW-leak-prompt

--- conftest.py
+++ conftest.py
@@ -28,6 +28,10 @@ def pytest_sessionstart(session):
 def setup_browser():
     # Add headless mode for CI compatibility
     options = webdriver.ChromeOptions()
+    prefs = {
+        "profile.password_manager_leak_detection_enabled": False
+    }
+    options.add_experimental_option("prefs", prefs)
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)    
     yield driver
