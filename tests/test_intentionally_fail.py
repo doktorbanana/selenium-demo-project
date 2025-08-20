3b3f0bbfb6d3ddc4f1f7f87b7db403cd993bb030
doktorbanana <peter.martin.hoevelmeyer@gmail.com>
2025-08-20 12:33:30 +0200

    Use Logger in intentionally failing test

--- tests/test_intentionally_fail.py
+++ tests/test_intentionally_fail.py
@@ -4,12 +4,11 @@ It is used for demonstration purposes and will only run when the
 --intentionally-fail option is set.
 """
 
-from pages.login_page import LoginPage
 import pytest
 
 
 @pytest.mark.intfail
-def test_intentionally_fail(setup_browser, request):
+def test_intentionally_fail(standard_login, test_case_log, request):
     """
     This test is intentionally failing for demonstration purposes.
     It is designed to fail when the --intentionally-fail option is not set.
@@ -19,15 +18,24 @@ def test_intentionally_fail(setup_browser, request):
                     "--intentionally-fail option. "
                     "It is intentionally failing for demonstration purposes.")
 
-    driver = setup_browser
-    driver.get("https://saucedemo.com")
-    login_page = LoginPage(driver)
-    inventory_page = login_page.login_expect_success(
-        "standard_user",
-        "secret_sauce")
+    test_case_log.set_description(
+        "This test intentionally fails."
+        " It's used to demonstrate how failing tests are handled."
+        )
+    test_case_log.set_severity("Low")
+    test_case_log.set_owner("QA")
+    test_case_log.set_group("Intentional Fail")
 
-    error_msg = "This test is intentionally failing " \
-                "for demonstration purposes. " \
-                f"We expect to be on the {inventory_page.title} when it fails."
+    test_case_log.start_step(1, "Login and navigate to Inventory Page")
+    inventory_page = standard_login
+    test_case_log.mark_step_finished(1)
+
+    test_case_log.start_step(2, "Do something bad!")
+    error_msg = (
+        "This test is intentionally failing "
+        "for demonstration purposes. "
+        f"We expect to be on the {inventory_page.driver.title}"
+        " page when it fails."
+    )
 
     assert False, error_msg
